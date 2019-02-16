63d45301b34aa114dacd861ee9e8d14d15daa444
PierreSymonPeralta <pierre@Pierres-MacBook-Pro.local>
2019-02-16 10:35:28 +0800

    Updates

--- precache-manifest.13d026bb8144ec07fa9e82ea9b3cb0c6.js
+++ precache-manifest.13d026bb8144ec07fa9e82ea9b3cb0c6.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/unsplash-app/static/js/runtime~main.c89c8ab2.js"
   },
   {
-    "revision": "8c520437f269009e90b1",
-    "url": "/unsplash-app/static/js/main.8c520437.chunk.js"
+    "revision": "dd29ecd95b3a2d44007d",
+    "url": "/unsplash-app/static/js/main.dd29ecd9.chunk.js"
   },
   {
     "revision": "c87d9c2274be38ff61fc",
     "url": "/unsplash-app/static/js/1.c87d9c22.chunk.js"
   },
   {
-    "revision": "8c520437f269009e90b1",
+    "revision": "dd29ecd95b3a2d44007d",
     "url": "/unsplash-app/static/css/main.de3b6a15.chunk.css"
   },
   {
-    "revision": "fe063d6e95031a0b3da15e3c061c7782",
+    "revision": "def554ad5247f1ec7eda24ae6587d964",
     "url": "/unsplash-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/unsplash-app/precache-manifest.a1ca013a909d6286dae4a4ba05bf6f19.js"
+  "/unsplash-app/precache-manifest.13d026bb8144ec07fa9e82ea9b3cb0c6.js"
 );
 
 workbox.clientsClaim();
